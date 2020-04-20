6784f12898701af947f832ef87d0fc24eb5cc53b
kamula <kamulaisaac@gmail.com>
2020-04-20 09:19:39 +0300

    admin area

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, choice
 
+admin.site.site_header = 'Pollster Admin'
+admin.site.site_title = 'Pollster Admin Area'
+admin.site.index_title = 'Welcome to pollster admin area'
+
 class ChoiceInline(admin.TabularInline):
     model = choice
     extra = 3
