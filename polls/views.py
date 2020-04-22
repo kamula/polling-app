b1c68f4dcbaf333550ced3164cf2d411e899909a
kamula <kamulaisaac@gmail.com>
2020-04-22 11:13:35 +0300

    details, results pages remaining

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Question, choice
+
 
 # Get questions and display them
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+
+# show specific questions and answers
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/result.html', {'question': question})
+
+
+# get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
